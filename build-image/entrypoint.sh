9f20a3f74770bb11e8d9bc2ee098dcb8967d9c48
George Oastler <goastler4@gmail.com>
2023-09-28 11:54:13 +0100

    delegate to ln to replace symlinks

--- build-image/entrypoint.sh
+++ build-image/entrypoint.sh
@@ -6,13 +6,8 @@ set -o pipefail # exit if any command in a pipe fails
 set -x # print commands
 
 # symlink the git and registry from cargo dir to the cache dir
-echo "Removing symlinks for cargo cache from previous runs..."
-rm $CARGO_HOME/registry || true
-rm $CARGO_HOME/git || true
-
-echo "Adding symlinks for cargo cache..."
-ln -s $CARGO_CACHE/registry $CARGO_HOME/registry
-ln -s $CARGO_CACHE/git $CARGO_HOME/git
+ln -sf $CARGO_CACHE/registry $CARGO_HOME/registry
+ln -sf $CARGO_CACHE/git $CARGO_HOME/git
 
 # run cargo contract with supplied args
 echo "Running cargo contract..."

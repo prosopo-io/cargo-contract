2d397565262a5adf3fe7074cd6f43f4954454972
George Oastler <goastler4@gmail.com>
2023-09-28 21:31:39 +0100

    remove symlink functionality, targetting using docker run mountpoint instead

--- build-image/entrypoint.sh
+++ build-image/entrypoint.sh
@@ -6,10 +6,11 @@ set -o pipefail # exit if any command in a pipe fails
 set -x # print commands
 
 # symlink the git and registry from cargo dir to the cache dir
-mkdir -p $CARGO_CACHE/registry
-mkdir -p $CARGO_CACHE/git
-ln -sf $CARGO_CACHE/registry $CARGO_HOME/registry
-ln -sf $CARGO_CACHE/git $CARGO_HOME/git
+# mkdir -p $CARGO_CACHE/registry
+# disabling git dir as this is just a copy of the src code for the git deps. Don't think this is needed
+# mkdir -p $CARGO_CACHE/git
+# ln -sf $CARGO_CACHE/registry $CARGO_HOME/registry
+# ln -sf $CARGO_CACHE/git $CARGO_HOME/git
 
 # run cargo contract with supplied args
 "$@"

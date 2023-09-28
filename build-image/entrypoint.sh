7a178dde78896d1fa0ff2afffff9901b19f6a3f1
George Oastler <goastler4@gmail.com>
2023-09-28 09:13:55 +0100

    fix removal of symlinks

--- build-image/entrypoint.sh
+++ build-image/entrypoint.sh
@@ -5,14 +5,15 @@ set -e # exit on error
 set -u # exit on undefined variable
 set -o pipefail # exit if any command in a pipe fails
 
-# create the git and registry dir in the cache if not already
-mkdir -p $CARGO_CACHE/registry
-mkdir -p $CARGO_CACHE/git
 # symlink the git and registry from cargo dir to the cache dir
-rm $CARGO_HOME/registry || true
-rm $CARGO_HOME/git || true
+echo "Removing symlinks for cargo cache from previous runs..."
+rm $CARGO_CACHE/registry || true
+rm $CARGO_CACHE/git || true
+
+echo "Adding symlinks for cargo cache..."
 ln -s $CARGO_CACHE/registry $CARGO_HOME/registry
 ln -s $CARGO_CACHE/git $CARGO_HOME/git
 
 # run cargo contract with supplied args
+echo "Running cargo contract..."
 cargo contract "$@"
